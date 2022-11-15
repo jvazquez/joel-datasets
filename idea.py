1411d2cf9ba08d7256723bd25b4594228699e37c
Jorge Omar Vázquez <jorgeomar.vazquez@gmail.com>
2022-11-15 10:02:30 -0300

    prototype idea

--- idea.py
+++ idea.py
@@ -10,13 +10,15 @@ dataset = [
 def run() -> Dict:
     accumulator = {}
 
-    for _id, city_name in dataset:
-        if city_name not in accumulator.keys():
-            accumulator[city_name] = [_id]
-        else:
-            accumulator[city_name].append(_id)
-
-    return accumulator
+    for value in df:
+        print(df)
+    # for _id, city_name in dataset:
+    #     if city_name not in accumulator.keys():
+    #         accumulator[city_name] = [_id]
+    #     else:
+    #         accumulator[city_name].append(_id)
+    # 
+    # return accumulator
 
 
 if __name__ == "__main__":
